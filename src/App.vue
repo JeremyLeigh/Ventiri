128dbe73bb3d6f8a47b1b815ed6fbf09784391c1
JeremyLeigh <le_jeremy@live.ca>
2022-05-23 14:16:02 -0400

    Initial Upload

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import FindFamiliar from '@/components/Find-Familiar.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  </header>
+  <div class="app-body">
+    <FindFamiliar />
+  </div>
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+#find-familiar {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.app-body
+{
+  width:100vw;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/components/Find-Familiar.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="wrapper">
+      <div class="creature-wrapper">
+        <div class="title-wrapper">
+          <h2>Name</h2>
+          <h4>Creature Type</h4>
+          <p>Creature Size/Category, Alignment</p>
+        </div>
+        <hr>
+        <div class="first-wrapper">
+          <p>Armor Class: Armor Class</p>
+          <p>Hit Points: Number (Number d6)</p>
+          <p>Speed: speed1, speed2, ...</p>
+        </div>
+        <hr>
+        <div class="stats-wrapper">
+          <ul>
+            <li>STR #</li>
+            <li>DEX #</li>
+            <li>CON #</li>
+            <li>INT #</li>
+            <li>WIS #</li>
+            <li>CHA #</li>
+          </ul>
+        </div>
+        <hr>
+        <div class="third-wrapper">
+          <p>Skills: Skill1 #, Skill2 #</p>
+          <p>Senses: Sense1 #, Sense2 #</p>
+          <p>Passive Perception: #</p>
+        </div>
+        <hr>
+        <div class="trait-wrapper">
+          <ul>
+            <li><h4>Trait1</h4><p>desc</p></li>
+            <li><h4>Trait2</h4><p>Trait1: desc</p></li>
+            <li><h4>Trait3</h4><p>Trait1: desc</p></li>
+          </ul>
+        </div>
+        <hr>
+        <div class="actions-wrapper">
+          <h3>Actions</h3>
+          <ul>
+            <li><h4>Action1</h4><p></p></li>
+            <li><h4>Action1</h4><p></p></li>
+            <li><h4>Action1</h4><p></p></li>
+          </ul>
+        </div>
+        <div>Basic Rules Section w/ Reveal Button</div>
+      </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+
+})
+</script>
+
+
+<style scoped>
+
+div
+{
+  padding-left: 1rem;
+  padding-right: 1rem;
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  width:100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+}
+
+ul
+{
+  list-style: none;
+  padding-left: 0;
+}
+
+.stats-wrapper ul
+{
+  padding-left: 0;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 4rem;
+  row-gap: 30px;
+  margin: 0 auto;
+}
+
+.stats-wrapper ul li
+{
+  width: 70px;
+  height: 70px;
+  padding: 0.5rem;
+  border-width: 2px;
+  border-color: brown;
+  border-style: solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.wrapper
+{
+  background-color: rgb(244, 242, 242);
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
